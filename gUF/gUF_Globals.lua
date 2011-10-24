4a84cc1d547234fa76a377572e890502980e5716
Grant Osborne <osborngs@gmail.com>
2011-10-24 22:14:31 +1030

    adding new folders

---
+++ gUF/gUF_Globals.lua
@@ -0,0 +1,57 @@
+--
+-- gUF_Globals
+--
+
+gUF.colors = {
+	["green_background"] = { r=0.0, g=0.7, b=0.0, a=0.3 },
+	["green_foreground"] = { r=0.0, g=0.7, b=0.0, a=0.6 },
+	["yellow_background"] = { r=0.7, g=0.7, b=0.0, a=0.3 },
+	["yellow_foreground"] = { r=0.7, g=0.7, b=0.0, a=0.6 },
+	["red_background"] = { r=0.7, g=0.0, b=0.0, a=0.3 },
+	["red_foreground"] = { r=0.7, g=0.0, b=0.0, a=0.6 },
+	["warrior"] = {r=1.0,g=0.0,b=0.0,a=0.6},
+	["mage"] = {r=1.0,g=0.0,b=0.7,a=0.6},
+	["rogue"] = {r=0.7,g=0.6,b=0.0,a=0.6},
+	["cleric"] = {r=0.0,g=0.7,b=0.3,a=0.6},
+	["black"] = { r=0.0, g=0.0, b=0.0, a=0.3 },
+	-- poisons/buffs here
+}
+
+gUF.fonts = {
+	["arial"] = "media/fonts/arial.ttf",
+	["arial_round"] = "media/fonts/arial_round.ttf",
+	["calibri"] = "media/fonts/calibri.ttf",
+	["calibri_bold"] = "media/fonts/calibri_bold.ttf"
+}
+
+gUF.bars = {
+	["ace"] = "media/bars/ace.tga",
+	["aluminium"] = "media/bars/aluminium.tga",
+	["banto"] = "media/bars/banto.tga",
+	["glaze"] = "media/bars/glaze.tga",
+	["lite"] = "media/bars/lite.tga",
+	["minimalist"] = "media/bars/minimalist.tga",
+	["otravi"] = "media/bars/otravi.tga",
+	["smooth"] = "media/bars/smooth.tga",
+	["striped"] = "media/bars/striped.tga"
+}
+
+gUF.iconTextures = {
+	-- icon overlays here (like square, rounded, etc)
+	-- for buffs/debuffs
+}
+
+gUF.roleIcons = {
+	["dps"] = "media/roles/dps.png",
+	["heal"] = "media/roles/heals.png",
+	["support"] = "media/roles/support.png",
+	["tank"] = "media/roles/tank.png"
+}
+
+gUF.icons = {
+	["in_combat"] = "media/icons/in_combat.png"
+}
+
+gUF.backgrounds = {
+	["backdrop"] = "media/backgrounds/gUF_backdrop.png"
+}
